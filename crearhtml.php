8136723fee29f83480003e4bbca25d85d3498cfc
reda-daw <rmerimi01@educarex.es>
2021-11-22 09:48:38 +0100

    1 commit

---
+++ crearhtml.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8"/>
+
+<title>Page Title</title>
+
+    <STYLE TYPE="text/css">
+
+        p { margin-bottom: 0in;}
+
+        td {
+
+            border:hidden;}
+
+        tr {
+
+            border:hidden;}
+
+        .contenedor{margin-left: 30px;margin-right:20px;}
+
+        .texto{  font-family: "GillSans Light, sans-serif"; font-size: 9pt;   text-align: justify;}
+
+        .clausula{font-weight:bold;}
+
+        .logoUE{}
+       
+    </STYLE>
+
+</head>
+
+<body>
+
+ 
+
+<div class="contenedor">    
+
+<table>
+<tr>
+
+    <td><img class="logoJE" SRC="logo_JE.jpg" WIDTH=195 HEIGHT=55 BORDER=0/></td>
+
+    <td style="width:410px"></td>
+
+    <td><IMG class="logoUE" SRC="logo_UE.jpg"   WIDTH=80 HEIGHT=65  BORDER=0/></td>
+</tr>    
+</table>    
+
+<div>
+
+<p LANG="es-ES" style="Arial Narrow, sans-serif, serif;font-weight:bold; font-size: 9pt;" ALIGN=CENTER>DIRECCIÓN GENERAL DE FORMACIÓN PROFESIONAL Y UNIVERSIDAD</p>
+
+<p LANG="es-ES" style="Arial Narrow, sans-serif, serif;font-weight:bold; font-size: 9pt;" ALIGN=CENTER>FORMACIÓN EN CENTROS DE TRABAJO
+
+    <br>CONVENIO CENTRO EDUCATIVO-EMPRESA
+
+    </p>
+
+<p LANG="es-ES"  ALIGN=CENTER></p>
+
+</div>
+
+<br>
+
+<table>
+
+    <tr>
+
+        <td style="border: 3px double #000000; padding: 0.1in"><p style="font-size: 8pt;text-align: left;"><b>CONVENIO ESPECÍFICO Nº________ (1)</b></p></td>
+
+        <td style="width:350px"></td>
+
+        <td><b>ANEXO 0 (Anverso)</b></td>
+
+    </tr>    
+
+</table>    
+
+   
+
+
+<p>
+
+   
+
+
+
+<table border="1">
+
+<tbody>
+
+<tr>
+
+<td>
+
+    <div style="float: left; width: 7in; height: 5px; border: 1px solid #000000; padding: 0.05in 0.1in; background: #ffffff"><b><font style="font-size: 8pt;text-align: left;">De una parte:</font></b></div>  
+
+ 
+
+<font face="GillSans Light, Segoe UI Semilight, sans-serif">
+
+    <font style="font-size: 8pt;text-align: left;">
+
+   
+
+<p>D./Dña.</p>
+
+
+<p>con D.N.I.  como Director/a del Centro educativo </p>
+
+
+<p>Código de Centro _________________ localizado en ___________________________________________________________________________</p>
+
+
+<p>provincia de ___________________, calle/plaza________________________________________________________________________________</p>
+
+
+<p>D.P._______________, con C.I.F. __________________________, Teléfono ___________________, Fax_ ______________________________,</p>
+
+
+<p>correo electrónico ____________________________________________________________________________________.</p>
+
+ </font>
+
+</font>
+
+</td>
+
+    </tr>
+
+<tr>
+
+<td><div style="float: left; width: 7in; height: 5px; border: 1px solid #000000; padding: 0.05in 0.1in; background: #ffffff"><b><font style="font-size: 8pt;text-align: left;">De otra parte:</font></b></div>
+
+</td>
+
+</tr>
+
+<tr>
+
+<td>
+
+<font face="GillSans Light, Segoe UI Semilight, sans-serif">
+
+    <font style="font-size: 8pt;text-align: left;">
+
+<p>D./Dña._____________________________________________________________ con D.N.I. ________________ como representante legal de la Empresa/Agrupación de empresas, o Entidad colaboradora ________________________________ _____________________________________________________ localizada en _________________________________ provincia de____________ ___________________________calle/plaza _____________________________________ DP. __________________________________________,</p>
+
+
+<p>con C.I.F. ________________________, Teléfono ________________________, Fax ____________________________.</p>
+
+
+<p>Correo electrónico ____________________________________.</p>
+
+
+         </font>
+
+</font>
+
+</td>
+
+</tr>
+
+
+
+</tbody>
+
+</table>
+
+  
+
+</p>
+
+
+<br />
+
+<p><table>
+
+<tbody>
+
+<tr>
+
+<td >
+
+
+</td>
+
+</tr>
+
+
+</tbody>
+
+</table>
+
+
+ 
+
+</p>
+
+
+<p style="GillSans Light , sans-serif;font-weight:bold;font-size: 11pt;text-align: center;">
+
+
+          EXPONEN
+
+
+    </p>
+
+<p style="GillSans Light , sans-serif;font-size: 9pt;text-align: justify;">
+
+        - Que ambas partes se reconocen recíprocamente capacidad y legitimidad parta convenir.
+
+<br>- Que el objetivo del presente CONVENIO es la colaboración entre las entidades a las que representan para el desarrollo de un <b>PROGRAMA FORMATIVO</b> de Formación en Centros de Trabajo, dirigido a los alumnos de Formación Profesional Reglada.
+
+<br>- Que dicha colaboración se fundamenta jurídicamente en el artículo 42, apartado 2, de la Ley Orgánica 2/2006 de Educación (LOE).
+
+     
+
+</p>
+
+   
+
+
+
+    <p style="GillSans Light , sans-serif;font-weight:bold;font-size: 11pt;text-align: center;">
+
+
+          ACUERDAN
+
+
+    </p>
+
+    <p style="GillSans Light , sans-serif;font-size: 9pt;text-align: justify;">
+
+         - Suscribir el presente CONVENIO de colaboración para el desarrollo del Módulo profesional de Formación en Centro de Trabajo, de acuerdo con la normativa vigente emitida por la Dirección General de Formación Profesional y Universidad para el curso escolar 20___- 20___, que ambas partes conocen y acatan, y en su caso, a lo estipulado en el convenio de colaboración del Consejo Superior de Cámaras de Comercio de España con el Ministerio de Educación y Cultura de fecha 10 de junio de 1999, y de conformidad con las cláusulas que figuran en la página 2 (al dorso de este documento).
+
+
+<br>- Al presente convenio se incorporarán, a lo largo del periodo de su vigencia, las relaciones nominales de alumnos acogidos al mismo (Anexo I), la programación de actividades formativas (Anexo II) a desarrollar por éstos en la empresa y los Documentos que faciliten el Seguimiento y Evaluación de los mismos.
+
+    </p>
+
+   
+
+   
+
+<table border="0" cellspacing="0" cellpadding="0" >
+
+<tbody>
+
+<tr>
+
+<td valign="top" > </td>
+
+<td colspan="2" valign="top" ><p>En ______________________ a ____ de ______________ de 20___</p></td>
+
+</tr>
+
+
+<tr>
+
+<td style="padding-right:35px;font-size: 9pt;text-align: center;" ><p>VºBº<br>
+
+EL/LA DELEGADO/A PROVINCIAL DE<br> EDUCACIÓN
+
+    </p>
+
+</td>
+
+<td style="padding-right:35px;font-size: 9pt;text-align: center;"><p>
+
+EL/LA DIRECTORA/A DEL CENTRO<br> EDUCATIVO
+
+    </p>
+
+</td>
+
+<td ><p><font style="font-size: 9pt;text-align: center;">EL/LA REPRESENTANTE DE LA EMPRESA
+
+    </font></p>
+
+</td>
+
+</tr>
+
+<tr>
+
+<td><br><br><br></td>    
+
+   
+
+<td></td>  
+
+   
+
+<td></td>  
+
+</tr>
+
+
+
+<tr>
+
+<td valign="bottom" ><p>Fdo.:</p></td>
+
+<td valign="bottom" ><p>Fdo.:</p></td>
+
+<td valign="bottom" ><p>Fdo.:</p></td>
+
+</tr>
+
+
+</tbody>
+
+</table>
+
+
+<p style="font-size: 9pt;text-align: justify;">(1) Registro provincial BA/número ó CC/número</p>
+</div>
+</body>
+
+</html>
